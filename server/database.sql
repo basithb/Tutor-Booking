bb345ff306a94cbc4c6f260e8a57c43a58dcc736
Basith <theonlybasith@gmail.com>
2022-02-08 23:04:31 +0530

    adds INSERT commands for tbl_login

--- server/database.sql
+++ server/database.sql
@@ -1,4 +1,8 @@
+-- Create Database tutorBooking
+
 CREATE DATABASE tutorBooking;
+
+-- Create tbl_login
 
 CREATE TABLE tbl_login(
 User_id uuid PRIMARY KEY DEFAULT
@@ -9,3 +13,7 @@ User_password VARCHAR(255) NOT NULL,
 User_type VARCHAR(255) NOT NULL,
 User_status VARCHAR(255) NOT NULL
 ); 
+
+-- Insert values into tbl_login
+
+INSERT INTO tbl_login (User_name, User_email, User_password, User_type, User_status) VALUES ('Admin', 'admin@tutor.com', 'admin', 'admin', 'active');

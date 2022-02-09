06a5268182aa9cce9d295cedd63f8699cd36dbc7
Basith <theonlybasith@gmail.com>
2022-02-09 14:57:26 +0530

    docs: checks if email is valid

---
+++ server/middleware/validInfo.js
@@ -0,0 +1,31 @@
+module.exports = function (req, res, next) {
+
+    //1. Destructures email, name, and password
+
+    const { email, name, password } = req.body;
+  
+    //2. Checking  if the email is valid
+    function validEmail(userEmail) {
+      return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail); // check if the email is following the regex (regular expression) pattern
+    }
+  
+    if (req.path === "/register") {
+      console.log(!email.length);
+      if (![email, name, password].every(Boolean)) {
+        return res.status(401).json("Missing Credentials");
+      } else if (!validEmail(email)) {
+        return res.status(401).json("Invalid Email");
+      }
+    } 
+    
+    
+    else if (req.path === "/login") {
+      if (![email, password].every(Boolean)) {
+        return res.status(401).json("Missing Credentials");
+      } else if (!validEmail(email)) {
+        return res.status(401).json("Invalid Email");
+      }
+    }
+  
+    next();
+  };

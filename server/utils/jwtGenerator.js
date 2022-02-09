bffca34022bea4aa96ffefc50bc5264ea5e19ee9
Basith <theonlybasith@gmail.com>
2022-02-09 11:51:15 +0530

    adds jwtGenerator() and signs the JWT token

---
+++ server/utils/jwtGenerator.js
@@ -0,0 +1,13 @@
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+
+function jwtGenerator(user_id){
+
+    const payload = {
+        user: user_id // takes user_id
+    }
+
+    return jwt.sign(payload, process.env.jwtSecret, {expiresIn : "1hr"}) // sign the JWT token by passing the payload data, the jwtSecret and also the expiration time. 
+}
+
+module.exports = jwtGenerator;

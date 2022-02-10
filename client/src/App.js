1b507fbba221bbc9c51ae1309e8fa22d7f5e5d7c
Basith <theonlybasith@gmail.com>
2022-02-10 11:38:00 +0530

    re-set up client side files and folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,13 @@
-import React, { Fragment } from "react";
-import "./App.css";
 
-import {Router, Route, Redirect} from "react-router-dom";
+import React, {Fragment} from 'react';
+import './App.css';
 
-//Importing Components
-
-import Dashboard from "./components/Dashboard";
-import Login from "./components/Login";
-import Register from "./components/Register";
-
-
-function App(){
-    return(
+function App() {
+  return (
     <Fragment>
-        <Router>
-            <div className="container">
-                <Route exact path ="/login" render={props => <Login {...props} />} />
-
-                <Route exact path ="/register" render={props => <Register {...props} />} />
-
-                <Route exact path ="/dashboard" render={props => <Dashboard {...props} />} />
-                
-            </div>
-        </Router>
+      
     </Fragment>
-    );
+  );
 }
+
+export default App;

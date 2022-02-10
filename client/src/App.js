57acfbeeaa481d1e0845890546e695c9218d139b
Basith <theonlybasith@gmail.com>
2022-02-10 18:11:13 +0530

    feat: sets up react dom router

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,31 @@
 import React, {Fragment} from 'react';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate
+} from "react-router-dom"; 
+
+// Importing Components
+
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
+
 function App() {
   return (
     <Fragment>
-      
+      <Router>
+        <div className="container">
+          <Routes>
+          <Route exact path="/login" element = { <Login />}/>
+          <Route exact path="/register" element = { <Register />}/>
+          <Route exact path="/dashboard" element = { <Dashboard />}/>
+          </Routes>
+        </div>
+      </Router>
     </Fragment>
   );
 }

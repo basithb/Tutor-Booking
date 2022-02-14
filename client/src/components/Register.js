912e8a4608e844018329a2684d432c498e261ef8
Basith <theonlybasith@gmail.com>
2022-02-14 21:52:16 +0530

    feat: adds link to /login

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,4 +1,5 @@
 import React, {Fragment, useState} from "react";
+import { Link } from "react-router-dom";
 
 const Register = ({setAuth}) => {
 
@@ -70,6 +71,7 @@ const Register = ({setAuth}) => {
                 </div>
                
             </form>
+            <Link to="/login">Login</Link>
         </Fragment>
     );
 };

a86416b2bc459e521458cf16b5d2a8d93e23cc8f
Basith <theonlybasith@gmail.com>
2022-02-17 23:42:16 +0530

    refactor: refactors code for landing page

--- client/src/components/landingPage/landingBody.js
+++ client/src/components/landingPage/landingBody.js
@@ -5,15 +5,15 @@ import './Landing.css';
 const LandingBody = () => {
     return (
         <Fragment>
-            <div className="text-center">
+            <div className="text-center container">
             <h1 className="landing-hero-text">Individual tutoring to reduce confusion and increase motivation </h1>
             <p className="landing-subhero-text"> 1-on-1 tutoring will make school easier and more enjoyable - register now</p>
-            <Link to="/register" className="btn btn-primary btn-md ">
+            <Link to="/register" className="btn btn-primary btn-lg px-4 landingBody-reg-button ">
                 Register
             </Link>
             </div>
             
-            <img className="float-bg-absolute" src="https://uploads-ssl.webflow.com/611cb284a5f4f500b86d82e8/61e07deb86cd11cc5bcd72c3_bg-float%20(1)%20(1)-p-1600.png" alt="" />
+            <img className="float-bg-absolute" src="https://uploads-ssl.webflow.com/611cb284a5f4f500b86d82e8/61e07deb86cd11cc5bcd72c3_bg-float%20(1)%20(1)-p-1600.png" alt="" width="auto" height="auto"/>
 
         </Fragment>
     );

e8eb1125b411df648cc9fd6192af029cd8ffc19c
Basith <theonlybasith@gmail.com>
2022-02-17 23:31:55 +0530

    feat: re-design login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,94 +1,102 @@
 import { parse } from "ipaddr.js";
-import React, {Fragment, useState} from "react";
+import React, { Fragment, useState } from "react";
+import Navbar from "./Navbar";
 import { Link } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import "./Login.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const Login = ({setAuth}) => {
-
-    
+const Login = ({ setAuth }) => {
     // Collecting state from these forms
     const [inputs, setInputs] = useState({
         // Default values
         email: "",
-        password: ""
-        });
+        password: "",
+    });
 
-     // Destructuring
-     const {email, password } = inputs;
+    // Destructuring
+    const { email, password } = inputs;
 
-     //onChange() function to change default values from the inputs i.e email ,name, and password.
- 
-     const onChange = (event) => {
-         setInputs({...inputs, [event.target.name] : event.target.value });
-     };  
-     
+    //onChange() function to change default values from the inputs i.e email ,name, and password.
 
-     //onSubmit() function to submit the inputs to our RESTful API to get the JWT Token
+    const onChange = (event) => {
+        setInputs({ ...inputs, [event.target.name]: event.target.value });
+    };
+
+    //onSubmit() function to submit the inputs to our RESTful API to get the JWT Token
 
     const onSubmitForm = async (event) => {
-
-        event.preventDefault(); // by default on clicking the 'Submit' button the page refreshes but with thepreventDefault() method; the page is prevented from refreshing. 
+        event.preventDefault(); // by default on clicking the 'Submit' button the page refreshes but with thepreventDefault() method; the page is prevented from refreshing.
 
         try {
-            
-            const body = {email, password};
+            const body = { email, password };
 
-            // Creating a fetch request 
-            const response = await fetch(
-                "http://localhost:5000/auth/login",
-                {
-                  method: "POST",
-                  headers: {
-                    "Content-type": "application/json"
-                  },
-                  body: JSON.stringify(body)
-                }
-              );
+            // Creating a fetch request
+            const response = await fetch("http://localhost:5000/auth/login", {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json",
+                },
+                body: JSON.stringify(body),
+            });
 
-        const parseRes = await response.json(); //parseResponse
+            const parseRes = await response.json(); //parseResponse
 
-        if(parseRes.token) {
+            if (parseRes.token) {
+                localStorage.setItem("token", parseRes.token);
+
+                setAuth(true);
+                toast.success("Login Successful!");
+            } else {
+                setAuth(false);
+                toast.error(parseRes);
+            }
+
             localStorage.setItem("token", parseRes.token);
 
             setAuth(true);
-            toast.success("Login Successful!");
-
-        }
-
-        else{
-            setAuth(false);
-            toast.error(parseRes);
-        }
-
-        localStorage.setItem("token", parseRes.token);
-
-        setAuth(true);
-
         } catch (error) {
             console.error(error.message);
         }
-        
-    } 
-    
+    };
+
     return (
         <Fragment>
-            <h1 className="text-center my-5">Login</h1>
+            <Navbar />
+            <div className="container py-5 h-100 login-container ">
+                <div className="row d-flex justify-content-center align-items-center h-100">
+                    <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                        <div className="card bg-white text-dark login-card">
+                            <div className="card-body p-5 text-center">
+                                <h2 className="mb-4 fw-bold">Welcome back!</h2>
 
-            <form onSubmit={onSubmitForm}>
-                <input type="email" name="email" placeholder="Enter your Email" className="form-control my-3"  value={email} onChange={event => onChange(event)}/>
+                                <form onSubmit={onSubmitForm}>
+                                    <div className="form-floating mb-4 ">
+                                        <input type="email" name="email" className="form-control input-email" id="floatingEmail" value={email}
+                                            onChange={(event) => onChange(event)} />
+                                        <label for="floatingEmail">Email ID</label>
+                                    </div>
 
-                <input type="password" name="password" placeholder="Enter your Password" className="form-control my-3"
-                value={password} onChange={event => onChange(event)}/>
+                                    <div className="form-floating ">
+                                        <input type="password" name="password" className="form-control input-password" id="floatingPassword" value={password}
+                                            onChange={(event) => onChange(event)} />
+                                        <label for="floatingPassword">Password</label>
+                                    </div>
 
-                <div className="d-grid">
-                <button className="btn btn-outline-success ">Submit</button>
+                                    <div className="d-grid">
+                                        <button className="btn btn-primary login-button">Log In</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div className="text-center mt-4">
+                            <p>Don't have an account? <Link to="/register" className="login-sub-text fw-bold">Find a Tutor</Link> here.</p>
+                        </div>
+                    </div>
                 </div>
-              
-            </form>
-            <Link to="/register">Register</Link>
+            </div>
         </Fragment>
-    )
-}
+    );
+};
 
 export default Login;

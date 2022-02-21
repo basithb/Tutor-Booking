2cabe873082075b464435a7457534c66180599a2
Basith <theonlybasith@gmail.com>
2022-02-21 16:19:46 +0530

    SQL Queries

--- server/database.sql
+++ server/database.sql
@@ -17,3 +17,20 @@ User_status VARCHAR(255) NOT NULL
 -- Insert values into tbl_login
 
 INSERT INTO tbl_login (User_name, User_email, User_password, User_type, User_status) VALUES ('Admin', 'admin@tutor.com', 'admin', 'admin', 'active');
+
+
+-- Create tbl_customer
+
+
+CREATE TABLE tbl_customer(
+customer_id integer NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+user_id uuid NOT NULL,
+customer_firstName VARCHAR(25) NOT NULL,
+customer_lastName VARCHAR(25) NOT NULL,
+customer_stateName VARCHAR(25) NOT NULL,
+customer_cityName VARCHAR(25) NOT NULL,
+FOREIGN KEY (user_id) REFERENCES tbl_login (user_id)
+);
+
+
+

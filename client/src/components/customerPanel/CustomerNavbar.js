9676a82e7ed7f7c3ea3bd3b401ff1457a3fd04e7
Basith <theonlybasith@gmail.com>
2022-03-03 23:31:58 +0530

    feat: adds navbar for customer dashboard

---
+++ client/src/components/customerPanel/CustomerNavbar.js
@@ -0,0 +1,17 @@
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+import "./CustomerNavbar.css";
+
+const CustomerNavbar = () => {
+    return (
+        <Fragment>
+            <header>
+                
+                <div class="min-height-300 bg-primary position-absolute w-100 fixed-top"></div>
+
+            </header>
+        </Fragment>
+    );
+};
+
+export default CustomerNavbar;

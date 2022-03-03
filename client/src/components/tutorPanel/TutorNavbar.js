65e13933003a7666984392714b9d6c19fff1f20f
Basith <theonlybasith@gmail.com>
2022-03-03 23:28:46 +0530

    feat: adds navbar component for tutor dashboard

---
+++ client/src/components/tutorPanel/TutorNavbar.js
@@ -0,0 +1,17 @@
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+import "./TutorNavbar.css";
+
+const TutorNavbar = () => {
+    return (
+        <Fragment>
+            <header>
+                
+                <div class="min-height-300 bg-primary position-absolute w-100 fixed-top"></div>
+
+            </header>
+        </Fragment>
+    );
+};
+
+export default TutorNavbar;

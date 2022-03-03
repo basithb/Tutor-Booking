102fe181f2de31f2ccef54e7c986f3ecc6fdf9e1
Basith <theonlybasith@gmail.com>
2022-03-03 23:37:41 +0530

    feat: contains admin, tutor & customer dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,49 +1,99 @@
-import React, {Fragment, useState, useEffect} from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import "./Dashboard.css";
+import AdminDashboard from "./adminPanel/AdminDashboard";
+import TutorDashboard from "./tutorPanel/TutorDashboard";
+import CustomerDashboard from "./customerPanel/CustomerDashboard";
 
-const Dashboard = ({setAuth}) => {
 
-//displaying states
-const [name, setName] = useState("")
+const Dashboard = ({ setAuth }) => {
 
-async function getName() {
-    try {
-   
-    const response = await fetch("http://localhost:5000/dashboard",
-    {
-        method: "GET",
-        headers: {token: localStorage.token}
-    });
-    
-    const parseRes = await response.json();
-    
-    setName(parseRes.user_name);
+    //Displaying states
+    const [name, setName] = useState("")
 
-    } catch (error) {
-        console.error(error.message);
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [user_type, setUserType] = useState("");
+
+    async function getName() {
+        try {
+
+            const response = await fetch("http://localhost:5000/dashboard",
+                {
+                    method: "GET",
+                    headers: { token: localStorage.token }
+                });
+
+            const parseRes = await response.json();
+
+            setName(parseRes.user_name);
+            console.log(parseRes);
+            setUserType(parseRes.user_type);
+
+
+            if (parseRes.user_type === "admin") {
+                setFirstName("Admin");
+                setLastName("Dashboard");
+            }
+
+            else if (parseRes.user_type === "customer") {
+
+                setFirstName(parseRes.customer_firstname);
+                setLastName(parseRes.customer_lastname);
+            }
+
+            else if (parseRes.user_type === "tutor") {
+
+                setFirstName(parseRes.tutor_firstname);
+                setLastName(parseRes.tutor_lastname);
+            }
+
+        } catch (error) {
+            console.error(error.message);
+        }
     }
-}
 
-//logout() function
-const logout = async (event) => {
+    //logout() function
+    const logout = async (event) => {
 
-event.preventDefault();
-localStorage.removeItem("token");
-setAuth(false);
-toast.success("Logged Out Successfully");
-}
+        event.preventDefault();
+        localStorage.removeItem("token");
+        setAuth(false);
+        toast.success("Logged Out Successfully");
+    }
 
-useEffect(() => {
-    getName();
-},[]);
+    useEffect(() => {
+        getName();
+    }, []);
 
     return (
+
+
         <Fragment>
-            <h1>Dashboard</h1>
+
+            {/* Log Out functionality works properly with the log out button below */}
+
+            {/* <h1>Dashboard</h1>
             <h3>Hello {name}</h3>
-            <button className="btn btn-primary" onClick={event => logout(event)}>Log Out</button>
-            
+            <button className="btn btn-primary" onClick={event => logout(event)}>Log Out</button> */}
+
+            {/* Actual Logic */}
+
+            {  
+                user_type === "admin" ? <AdminDashboard /> : user_type === "customer" ? <CustomerDashboard /> : <TutorDashboard />
+            }
+
+
+
+            {/* Testing purposes (to be deleted later) */}
+
+            {/* <TutorDashboard /> */}
+            {/* <CustomerDashboard/> */}
+            {/* <AdminDashboard /> */}
+
+
         </Fragment>
     )
 }

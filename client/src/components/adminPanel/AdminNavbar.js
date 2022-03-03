6de3c43c9534b187f4572061f49b154dd69cae2f
Basith <theonlybasith@gmail.com>
2022-03-03 23:34:39 +0530

    feat: adds navbar for admin dashboard

---
+++ client/src/components/adminPanel/AdminNavbar.js
@@ -0,0 +1,17 @@
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+import "./AdminNavbar.css";
+
+const AdminNavbar = () => {
+    return (
+        <Fragment>
+            <header>
+                
+                <div class="min-height-300 bg-primary position-absolute w-100 fixed-top"></div>
+
+            </header>
+        </Fragment>
+    );
+};
+
+export default AdminNavbar;

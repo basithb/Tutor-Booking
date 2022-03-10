d32d27645a17ee149764f8ee3d90be31e7bf548b
Basith <theonlybasith@gmail.com>
2022-03-10 23:28:29 +0530

    feat: adds common backdrop for all panels

---
+++ client/src/components/backdropPanel/PanelBackdrop.js
@@ -0,0 +1,17 @@
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+import "./PanelBackdrop.css";
+
+const PanelBackdrop = () => {
+    return (
+        <Fragment>
+            <header>
+                
+                <div class="min-height-300 bg-primary position-absolute w-100 "></div>
+
+            </header>
+        </Fragment>
+    );
+};
+
+export default PanelBackdrop;

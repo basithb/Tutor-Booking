522cf967f99b6835fe7fe313260a2b303d2479ac
Basith <theonlybasith@gmail.com>
2022-04-06 11:25:12 +0530

    feat: adds admin reports page

---
+++ client/src/components/adminPanel/Reports.js
@@ -0,0 +1,38 @@
+import React, { Fragment, useState, useEffect } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import "./Reports.css";
+import PanelBackdrop from "../backdropPanel/PanelBackdrop";
+import AdminSidebar from "./AdminSidebar";
+import { Link } from "react-router-dom";
+import AdminNavbar from "./AdminNavbar";
+
+const AdminReports = (props, { setAuth }) => {
+    try {
+
+    } catch (error) {
+        console.error(error.message);
+    }
+
+    const isActive = "admin-reports";
+
+    return (
+        <Fragment>
+            <body>
+                <PanelBackdrop />
+                <AdminNavbar />
+                <AdminSidebar setAuth={setAuth} isActive={isActive} />
+                <main className="main-content position-relative border-radius-lg">
+
+                   
+                </main>
+
+
+            </body>
+        </Fragment>
+    )
+
+
+}
+
+export default AdminReports;
